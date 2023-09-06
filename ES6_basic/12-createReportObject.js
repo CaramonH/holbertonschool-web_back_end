fd32b4b323745dbc32c621c9e04ee91b52785798
CaramonH <caramonh@yahoo.com>
2023-09-06 17:22:20 +0000

    Task 12

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,7 +1,7 @@
 export default function createReportObject(employeesList) {
   return {
-    allEmployees: { ...employeeList },
-    getNumberOfDepartments(employeeList) {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(employeesList) {
       return Object.keys(employeesList).length;
     },
   };
