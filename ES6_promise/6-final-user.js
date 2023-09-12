916c4874df65fe203601c567b80357d82edd8246
CaramonH <caramonh@yahoo.com>
2023-09-12 19:55:40 +0000

    interum

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,11 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName,
+) {
+  const status = {};
+  await signUpUser(firstName, lastName)
+    .then(async (data) => {
