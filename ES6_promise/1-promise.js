45d7199680119bc57da00eecc927b81572c1d36c
CaramonH <caramonh@yahoo.com>
2023-09-12 18:20:39 +0000

    Task 1

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,6 +1,12 @@
-export default function getListStudentIds(list) {
-  if (!Array.isArray(list)) {
-    return [];
-  }
-  return list.map((student) => student.id);
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
 }
